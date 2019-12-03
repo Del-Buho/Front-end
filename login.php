418cba6426d59dec881f253db137b237722b40c8
MichaelVelarde <Michael.velarde.kubber@gmail.com>
2019-12-03 09:02:03 -0400

    primer login

---
+++ login.php
@@ -0,0 +1,33 @@
+<HTML class="bg-black">
+	<head>
+        <meta charset="UTF-8">
+        <!-- <title>AdminLTE | Log in</title> -->
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+		<title>Senasag Web</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css"  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/admin.css" media=""/>		 	
+    </head>
+    <body class="bg-black">
+
+				
+		
+		        <div class="form-box" id="login-box">
+            <div class="header" style="background-color:#008f39;">  Senasag Login </div>
+            <form >
+                <div class="body bg-gray">
+                    <div class="form-group">
+                        <input type="text" name="user[username]" class="form-control" placeholder="Usuario" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="user[password]" class="form-control" placeholder="Contrase&ntilde;a" />
+                    </div>          
+                                    </div>
+                <div class="footer">                                                               
+                    <button type="submit" class="btn bg-green btn-block" >Acceder</button>
+                </div>
+            </form>
+
+        </div>
+    </body>
+</HTML>	                                                                                                                                                                                                                                                                                                                                                                              
